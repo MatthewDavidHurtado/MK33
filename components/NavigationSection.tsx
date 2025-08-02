f3533ab162d90dbad00f891c113f0291fbc89e40
MALCOLM KINGLEY <71328086+MatthewDavidHurtado@users.noreply.github.com>
2025-08-02 05:28:59 -0700

    Adjust scroll position to center treatment area

--- components/NavigationSection.tsx
+++ components/NavigationSection.tsx
@@ -142,7 +142,7 @@ const NavigationSection: React.FC = () => {
                             if (treatmentSection) {
                                 treatmentSection.scrollIntoView({ 
                                     behavior: 'smooth', 
-                                    block: 'center' 
+                                    block: 'start' 
                                 });
                             }
                         }}
