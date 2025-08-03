414b6e25abda4df24d937b8b05ba9e4dcac7c586
MALCOLM KINGLEY <71328086+MatthewDavidHurtado@users.noreply.github.com>
2025-08-03 13:23:46 -0700

    Fix NavigationSection component completely

--- components/NavigationSection.tsx
+++ components/NavigationSection.tsx
@@ -103,7 +103,6 @@ const NavigationSection: React.FC = () => {
                     </p>
                 </div>
                 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     <NavigationCard
                         title="Sacred Library"
