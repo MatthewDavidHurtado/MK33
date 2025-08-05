5ca0afbbea4bd7267cb29f3fc300172ba7b4cc1b
MALCOLM KINGLEY <71328086+MatthewDavidHurtado@users.noreply.github.com>
2025-08-05 11:43:08 -0700

    Add MK Private Client Portal link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -106,8 +106,9 @@ const Footer: React.FC = () => {
                     <div className="flex justify-center items-center gap-x-4 gap-y-1 flex-wrap !mt-4">
                         <a href="https://app.ardrive.io/#/file/1f5d713c-d94e-4036-b6fe-faf29a2d7505/view" target="_blank" rel="noopener noreferrer" className="hover:underline hover:text-gold-400 text-slate-400 transition-colors">Ministry Documentation</a>
                         <span className="text-slate-600">|</span>
-                        <span className="text-slate-600">|</span>
                         <a href="https://malcolm-s-workspace-759385455270.us-west1.run.app" target="_blank" rel="noopener noreferrer" className="hover:underline hover:text-gold-400 text-slate-400 transition-colors">MK Workspace</a>
+                        <span className="text-slate-600">|</span>
+                        <a href="https://copy-2-of-master-copy-of-with-scan-biological-cod-759385455270.us-west1.run.app/" target="_blank" rel="noopener noreferrer" className="hover:underline hover:text-gold-400 text-slate-400 transition-colors">MK Private Client Portal</a>
                     </div>
                     
                     <p className="!mt-6">
