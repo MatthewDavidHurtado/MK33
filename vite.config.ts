eecd83f5fa8473dc49898449ba67d68838111d08
MALCOLM KINGLEY <71328086+MatthewDavidHurtado@users.noreply.github.com>
2025-09-04 11:40:12 -0700

    Fix Vite routing configuration for success page

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    historyApiFallback: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
     }
-  }
+  },
 });
