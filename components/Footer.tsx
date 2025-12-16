9fdc0a696fb2cb7a98b01ce93330506e318dbbaf
MALCOLM KINGLEY <71328086+MatthewDavidHurtado@users.noreply.github.com>
2025-12-16 16:30:03 -0600

    Updated Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -95,12 +95,7 @@ const Footer: React.FC = () => {
 
                     <div className="aspect-video w-full bg-slate-900 rounded-lg overflow-hidden shadow-xl relative border-2 border-gold-400/30 flex items-center justify-center">
                         <div className="text-center">
-                            <div className="bg-gold-400/10 rounded-full p-6 inline-block mb-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-16 h-16 text-gold-400">
-                                    <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.647c1.295.742 1.295 2.545 0 3.286L7.279 20.99c-1.25.72-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
-                                </svg>
-                            </div>
-                            <p className="text-slate-400 text-lg font-semibold">FREE RESOURCES</p>
+                            <p className="text-gold-400 text-3xl md:text-4xl font-bold font-cinzel">COMING SOON!</p>
                         </div>
                     </div>
                 </div>
